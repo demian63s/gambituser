42570a891aa22afed86db41e8e29ffdd8f708431
Damian Sanguino <demian63s@gmail.com>
2023-07-20 11:21:45 -0300

    Creacion inicio de lambda

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+
+	"github.com/demian63s/gambituser/awsgo"
+
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, event events.CognitoEventUserPoolsPostConfirmation) (events.CognitoEventUserPoolsPostConfirmation, error) {
+	awsgo.InicializoAws()
+}
